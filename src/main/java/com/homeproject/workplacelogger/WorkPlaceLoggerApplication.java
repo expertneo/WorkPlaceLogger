08dde042a2ec733a197d9f2d11f663b387872a30
Bence Papp <bence.papp.hu@gmail.com>
2018-06-13 21:08:23 +0200

    initial commit

---
+++ src/main/java/com/homeproject/workplacelogger/WorkPlaceLoggerApplication.java
@@ -0,0 +1,12 @@
+package com.homeproject.workplacelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPlaceLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPlaceLoggerApplication.class, args);
+	}
+}
